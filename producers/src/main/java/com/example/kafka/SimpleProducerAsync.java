2f6099af9061bcad49ee7f4ab7edc5b3c00c7f5d
yoonsik <ys.shin@okestro.com>
2024-02-27 20:42:12 +0900

    producer 구현 with Sync & Async

---
+++ producers/src/main/java/com/example/kafka/SimpleProducerAsync.java
@@ -0,0 +1,47 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.Future;
+
+public class SimpleProducerAsync {
+    public static void main(String[] args) {
+        String topic = "simple-topic";
+
+        // KafkaProducer config setting
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 생성
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // ProducerRecord 생성
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "hello world!");
+
+        // send()가 된다고 바로 record를 전송하지 않는다
+        // onCompletion()을 실제로 수행하는 스레드는 sender 스레드
+        Future<RecordMetadata> result = kafkaProducer.send(record, (metadata, exception) -> {
+            if (exception == null) {
+                System.out.println(metadata.partition());
+                System.out.println(metadata.offset());
+                System.out.println(metadata.timestamp());
+            } else {
+                exception.printStackTrace();
+            }
+        });
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        kafkaProducer.close();
+
+    }
+}
---
+++ producers/src/main/java/com/example/kafka/SimpleProducerSync.java
@@ -0,0 +1,46 @@
+package com.example.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+@Slf4j
+public class SimpleProducerSync {
+    public static void main(String[] args) {
+        String topic = "simple-topic";
+
+        // KafkaProducer config setting
+        Properties props = new Properties();
+        props.setProperty("bootstrap.servers", "localhost:9092");
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // KafkaProducer 생성
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(props);
+
+        // ProducerRecord 생성
+        ProducerRecord<String, String> record = new ProducerRecord<>(topic, "hello world!");
+
+        // send()가 된다고 바로 record를 전송하지 않는다
+        try {
+            RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+            System.out.println(recordMetadata.partition());
+            System.out.println(recordMetadata.offset());
+            System.out.println(recordMetadata.timestamp());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        } finally {
+            kafkaProducer.close();
+        }
+    }
+}
